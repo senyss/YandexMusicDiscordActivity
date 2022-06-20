90b5098b076a6a636c7ef28b16bd977b0ade3e64
senyss <dansmitr312@gmail.com>
2022-06-20 15:48:40 +0300

    remove useless

--- NM.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-cls
-cd C:\Users\OwO\Desktop\YandexMusicDiscordActivity
-java -Xmx20m -jar .\build\libs\YandexMusicDiscordActivity-1.0-SNAPSHOT-all.jar
--- NMR.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-cls
-cd C:\Users\OwO\Desktop\YandexMusicDiscordActivity
-.\gradlew.bat build
